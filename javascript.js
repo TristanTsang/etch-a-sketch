b5bdbe55bc0d99a5b98380873b7d25574f504633
Tristan Tsang <tristantsang9786@gmail.com>
2024-07-23 11:36:14 -0400

    Create Etch A Sketch layout
    Create the basic structure of the Etch A Sketch.
    Create basic styling for Etch A Sketch elements.
    Implement functions to create Etch A Sketch pixels and rows.

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const WIDTH = 960;
+let numRows = 16; // # of squares per side
+const GAP_SIZE = 5;
+let divSize = WIDTH / numRows - GAP_SIZE;
+const container = document.querySelector(".etch-a-sketch-container");
+
+function row() {
+  const row = document.createElement("div");
+  row.classList.add("etch-a-sketch-row");
+  for (let i = 0; i < numRows; i++) {
+    row.appendChild(pixel());
+  }
+
+  return row;
+}
+
+function pixel() {
+  const pixel = document.createElement("div");
+  pixel.classList.add("pixel");
+  pixel.setAttribute(
+    "style",
+    "height: " + divSize + "px; width:" + divSize + "px;"
+  );
+  return pixel;
+}
+
+function start(num) {
+  for (let i = 0; i < num; i++) {
+    container.appendChild(row());
+  }
+}
+start(numRows);
